b91b4030d1c6881d5e6269335f66f2ee67b2e51f
Lewis Sparlin <lsparlin@users.noreply.github.com>
2017-05-26 08:36:54 -0500

    Just removing NetlifyFooter file (#9)
    * Some renaming of style classes to match JS names
    
    * Removed .jsx extension from imports
    
    * Caonical rel in head and styling of tag labels
    
    * Cleanup in BlogListing.jsx
    
    * Reorganized to have DocumentList.jsx instead of blog specific list
    
    * Added Disqus commeting on blogs
    
    * Using configuration for Disqus shortname
    
    * Better Disqus configuration
    
    * Fixed stuff
    
    * Delete NetlifyFooter.jsx

--- src/js/components/NetlifyFooter.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default () => (
-  <div className="NetlifyFooter footer">
-    <a href="https://www.netlify.com">
-      <img src="https://www.netlify.com/img/global/badges/netlify-color-bg.svg" />
-    </a>
-  </div>
-)
