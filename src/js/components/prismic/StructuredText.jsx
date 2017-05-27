8f303d314568ae5a6c2836da4fa85cdb8b0333d7
Lewis Sparlin <lewis.sparlin@icloud.com>
2017-05-26 20:17:46 -0500

    some styling and code slice fix

--- src/js/components/prismic/StructuredText.jsx
+++ src/js/components/prismic/StructuredText.jsx
@@ -11,6 +11,7 @@ class StructuredText extends React.Component {
     // If the text contains spans for rich text - I will :( use this unsafe operation so I don't re-invent the wheel
     var isTooComplexForMe = this.structuredText.blocks.filter(block => block.spans && !!block.spans.length).length
     if (isTooComplexForMe) {
+      console.log('too complex', this.structuredText.asText())
       return ( <div className="StructuredText" dangerouslySetInnerHTML={{__html: this.structuredText.asHtml()}}></div> )
     }
 
